be7da3e3bffb0e6529b596f893d6e299a76dcd01
nickpelling <nickpelling@nickpelling.com>
2019-10-14 21:07:00 +0100

    Tweaking tt loop

--- src/tt.cpp
+++ src/tt.cpp
@@ -131,14 +131,20 @@ TTEntry* TranspositionTable::probe(const Key key, bool& found) const {
 
   // Find an entry to be replaced according to the replacement strategy
   TTEntry* replace = tte;
+  int bestValue = replace->depth8 - ((263 + generation8 - replace->genBound8) & 0xF8);
   for (int i = 1; i < ClusterSize; ++i)
+  {
       // Due to our packed storage format for generation and its cyclic
       // nature we add 263 (256 is the modulus plus 7 to keep the unrelated
       // lowest three bits from affecting the result) to calculate the entry
       // age correctly even after generation8 overflows into the next cycle.
-      if (  replace->depth8 - ((263 + generation8 - replace->genBound8) & 0xF8)
-          >   tte[i].depth8 - ((263 + generation8 -   tte[i].genBound8) & 0xF8))
+      int thisValue = tte[i].depth8 - ((263 + generation8 - tte[i].genBound8) & 0xF8);
+      if (  bestValue > thisValue )
+      {
           replace = &tte[i];
+          bestValue = thisValue;
+      }
+  }
 
   return found = false, replace;
 }
